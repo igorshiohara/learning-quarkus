fd5d44c3f7e39721840aff89c3bab9fe4876624e
Igor Shiohara <igor.shiohara@skipthedishes.ca>
2021-05-21 16:15:09 -0500

    Implementation with Hibernate ORM

--- quarkus-command-app/src/main/java/ca/igorshiohara/CommandRunner.java
+++ quarkus-command-app/src/main/java/ca/igorshiohara/CommandRunner.java
@@ -1,11 +1,13 @@
 package ca.igorshiohara;
 
+import io.quarkus.arc.Arc;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -20,7 +22,7 @@ public class CommandRunner implements QuarkusApplication {
     private static final Logger LOGGER = Logger.getLogger(CommandRunner.class);
 
     @Inject
-    DataSource dataSource;
+    EntityManager entityManager;
 
     @ConfigProperty(defaultValue = "Anonymous", name="application.greeting.recipient")
     private String recipient;
@@ -29,25 +31,11 @@ public class CommandRunner implements QuarkusApplication {
     public int run(String... args) throws Exception {
         LOGGER.debug("Starting application");
 
-        String sql = "SELECT NAME, ROOM_NUMBER, BED_INFO FROM ROOM";
-
-        List<Room> rooms = new ArrayList<>();
-        try {
-            Connection connection = dataSource.getConnection();
-            try(Statement stmt = connection.createStatement()) {
-                ResultSet rs = stmt.executeQuery(sql);
-                while (rs.next()) {
-                    rooms.add(new Room(rs.getString("NAME"),
-                                       rs.getString("ROOM_NUMBER"),
-                                       rs.getString("BED_INFO")));
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        Arc.container().requestContext().activate();
+        List<Room> rooms = entityManager.createQuery("select r from Room r", Room.class).getResultList();
         rooms.forEach(System.out::println);
 
+        Arc.container().requestContext().deactivate();
         LOGGER.debug("Completing application");
         return 0;
     }
--- quarkus-command-app/src/main/java/ca/igorshiohara/Room.java
+++ quarkus-command-app/src/main/java/ca/igorshiohara/Room.java
@@ -1,9 +1,28 @@
 package ca.igorshiohara;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROOM")
 public class Room {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ROOM_ID")
+    private long id;
+
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "ROOM_NUMBER")
     private String roomNumber;
+
+    @Column(name = "BED_INFO")
     private String bedInfo;
 
     public Room() {
